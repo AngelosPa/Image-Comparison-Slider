255cad77e6b5057fda15f3891984dfe14ea36d89
angelo.p <mrpalaiologos@gmail.com>
2021-05-12 14:27:22 +0200

    i added a promise

--- index.js
+++ index.js
@@ -1,8 +1,53 @@
 const slider = document.querySelector(".slider input");
-const img = document.querySelector(".image-2");
+const img = document.querySelector("image-2");
 slider.oninput = () => {
   let slidervali = slider.value;
-  //slider.style.left = slidervali + "%";
-  //img.style.width = slidervali + "%";
+
   img.style.opacity = slidervali + "%";
 };
+
+const checkPromise = new Promise((resolve, reject) => {
+  let x = true;
+  if (x) {
+    resolve("everything is cool");
+  } else {
+    reject("sorry, it's not cool");
+  }
+});
+
+checkPromise
+  .then((message) => {
+    console.log(`I am then and ${message}`);
+  })
+
+  .catch((err) => {
+    console.log(`I am catch and ${err}`);
+  });
+console.log("Hi, I am Hadi");
+// Promisify setTimeout
+
+function delay(ms) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve();
+    }, ms);
+  });
+}
+let firstpic = document.querySelector(".image-1");
+let secpic = document.querySelector(".image-2");
+console.log(firstpic);
+let ms = 3000;
+function doSomething() {
+  let text = document.querySelector("h3");
+  text.innerHTML = ``;
+
+  let wheel = document.querySelector("#tria");
+  let inputball = document.querySelector(".input");
+  inputball.classList.remove("input");
+
+  wheel.remove("wheel");
+  firstpic.classList.remove("ena");
+  secpic.classList.remove("dyo");
+}
+
+delay(ms).then(doSomething);
